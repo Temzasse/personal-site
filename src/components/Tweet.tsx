a36f67712ed22ed201ac668db0bbf6f3b436885b
Teemu Taskula <themetor@gmail.com>
2024-11-16 15:04:18 +0200

    Add Tweet component

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,31 @@
+import { Suspense } from "react";
+import { unstable_cache } from "next/cache";
+import { TweetSkeleton, EmbeddedTweet, TweetNotFound } from "react-tweet";
+import { getTweet as _getTweet } from "react-tweet/api";
+
+const getTweet = unstable_cache(
+  async (id: string) => _getTweet(id),
+  ["tweet"],
+  { revalidate: 3600 * 24 }
+);
+
+async function TweetEmbed({ id }: { id: string }) {
+  try {
+    const tweet = await getTweet(id);
+    return tweet ? <EmbeddedTweet tweet={tweet} /> : <TweetNotFound />;
+  } catch (error) {
+    console.error(error);
+    return <TweetNotFound error={error} />;
+  }
+}
+
+export function Tweet({ id }: { id: string }) {
+  if (!id) return null;
+  return (
+    <Suspense fallback={<TweetSkeleton />}>
+      <div className="tweet" data-theme="dark">
+        <TweetEmbed id={id} />
+      </div>
+    </Suspense>
+  );
+}
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from "mdx/types";
 
 import { mdxStyles } from "./styles/mdx";
 import { HeroImage } from "./components/HeroImage";
+import { Tweet } from "./components/Tweet";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -10,5 +11,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       <div className={`${mdxStyles} blog-post`}>{children}</div>
     ),
     HeroImage: (props) => <HeroImage {...props} />,
+    Tweet: (props) => <Tweet {...props} />,
   };
 }
